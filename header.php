273b1ef078660cd42f2a79154663852b31c8d95f
Grant Imbo <grant.imbo@gmail.com>
2019-10-30 13:17:00 +0800

    update-font-weight

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 
 		<link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
 	    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-		<link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Raleway:700" rel="stylesheet">
 
 
 		<div id="fb-root"></div>
