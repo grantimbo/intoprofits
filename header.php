b3f1e9e867c05f6b2fd9a007bac69a7ab7b38ce6
Grant Imbo <grant.imbo@gmail.com>
2019-11-08 18:48:30 +0800

    icon update

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">
 		
 
-		<link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+		<link href="<?php echo get_template_directory_uri(); ?>/img/icons/icon.ico" rel="shortcut icon">
 	    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 		<link href="https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap" rel="stylesheet">
 
