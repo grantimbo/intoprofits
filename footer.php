5ec202f8c7137b9720ed5f9469045418c2a6d037
Grant Imbo <grant.imbo@gmail.com>
2019-11-11 09:54:10 +0800

    footer updates

--- footer.php
+++ footer.php
@@ -1,13 +1,33 @@
 	<?php wp_footer(); ?>
-	<footer class="container">
-		<div class="wrap clear">
-			<div class="copyright">
-				<p style="margin-bottom: 6px;">&copy; <?php echo date('Y'); ?> IntoProfits.com</p>
-				<div style="text-align: center;">
-					<a href="<?php echo site_url(); ?>/privacy-policy">Privacy &amp; Legal</a>
+	<footer>
+
+		<div class="primary">
+			<div class="wrap clear">
+				<img src="<?php echo bloginfo('template_url'); ?>/img/footer-logo.png" alt="Apex Seller">
+				<?php footer_nav(); ?>
+			</div>
+		</div>	
+
+		<div class="secondary clear">
+			<div class="wrap clear">
+				<div class="copyright">
+					<p>&copy; <?php echo date('Y'); ?> IntoProfits.com</p>
+					<div class="copyright-links">
+						<a style="margin-right: 20px;" href="<?php echo site_url(); ?>/privacy-policy">Privacy &amp; Legal</a>
+						<a href="<?php echo site_url(); ?>/contact">Contact</a>
+					</div>
+				</div>
+	
+				<div class="social">
+					<a href="https://app.intoprofits.com/login" target="_blank">Login</a>
+					<div class="social-icons">
+						<a class="icon-youtube" target="_blank" href="https://www.youtube.com/channel/UCOhQ7IdiVx4IKDOKRKl7RtA"></a>
+						<a class="icon-facebook" target="_blank" href="https://business.facebook.com/danielaudunssonlive"></a>
+						<a class="icon-twitter" target="_blank" href="https://twitter.com/DanielAudunsson"></a>
+						<a class="icon-linkedin" target="_blank" href="https://www.linkedin.com/in/daniel-audunsson-81b3b27b/"></a>
+					</div>
 				</div>
 			</div>
-			<?php footer_nav(); ?>
 		</div>
 	</footer>
 	</body>
