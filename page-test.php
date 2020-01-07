6e957c3824906497900d78663c5b33ad54388a76
Grant Imbo <grant.imbo@gmail.com>
2020-01-07 09:54:33 +0800

    test

--- page-test.php
+++ page-test.php
@@ -1,6 +1,6 @@
 
 
 <main class="container">
-<?php echo do_shortcode('[contact-form-7 id="915" title="Contact form 1"]'); ?>
+<?php echo do_shortcode('[contact-form-7 id="836" title="Contact form 1"]'); ?>
 </main>
 
