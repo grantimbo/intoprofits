d7c6052a8179f7049386d20d62373daa68fdd28d
Grant Imbo <grant.imbo@gmail.com>
2020-01-07 15:33:00 +0800

    file upload

--- page-test.php
+++ page-test.php
@@ -1,6 +1,81 @@
 
 
+<?php /*
+<aside class="sidebar">
+
+    <section class="steps">
+        <p class="active">Step 1 - Rate Program</p>
+        <p>Step 2 - Record Testimony</p>
+        <p>Step 3 - Upload Testimony</p>
+        <p>Step 4 - Answer Question</p>
+    </section>
+    <!-- /steps -->
+
+    <section class="progress">
+        <p>Your Progress</p>
+        <div class="progress-bar 20">20%</div>
+    </section>
+    <!-- /progress -->
+</aside>
+
 <main class="container">
-<?php echo do_shortcode('[contact-form-7 id="836" title="Contact form 1"]'); ?>
+    
+    <section class="rate-program">
+        <h1>How Would You Rate The Program?</h1>
+        <p>Select a rating from 1 to 5 with being bad to 5 being awesome.</p>
+        <div class="star-rate">
+            <span data-rate="1" title="Give 1 stars">★</span>
+            <span data-rate="2" title="Give 2 stars">★</span>
+            <span data-rate="3" title="Give 3 stars">★</span>
+            <span data-rate="4" title="Give 4 stars">★</span>
+            <span data-rate="5" title="Give 5 stars">★</span>
+        </div>
+        <button>Next</button>
+    </section>
+    <!-- /rate-program -->
+
+    <section class="record-testimony">
+        <h1>Record Your Video Testimonial</h1>
+        <p>The next step is to record your video testimonial where you share your experience of the Apex Seller program. The tutorial video below explains how to shoot your video and what structure/format to follow.</p>
+        <video src=""></video>
+        <button>Next</button>
+    </section>
+    <!-- /record-testimony -->
+
+    <section class="upload-testimony">
+        <h1>Upload Your Video Testimonial File</h1>
+        <p>The next step is to upload your video file. This should be a .mp4, .mov or .avi video file and it should be less than 500mb in size. If you have issues uploading your video file please use <a href="#">Handbrake</a> to compress your video as a "web optimized .mp4".</p>
+        <video src=""></video>
+        <button>Next</button>
+    </section>
+    <!-- /upload-testimony -->
+
+
+<?php // echo do_shortcode('[contact-form-7 id="836" title="Contact form 1"]'); ?>
 </main>
 
+*/ ?>
+
+ <style type="text/css">
+    #gdwidget-upload-slot-24146db4eb {
+        margin: 10px auto; width:90%; border:1px solid #aaa;
+        cursor: pointer; text-align: center; font-family: arial; font-weight: bold;
+        padding: 40px;
+        background: #fff;
+    }
+    </style>
+    <script>
+        var uploadToken = '$2y$13$nsxC77ZJqW27JkwnXQg1gOdWRAA68SR1UnpI2MlWmpc8klbQOiK.W';
+        var uploadSlot = 'gdwidget-upload-slot-24146db4eb';
+        // Change the below if you would like to have your own messages
+        var initTxt = "<img src='https://gdwidget.com/img/icon.svg' style='width:50px;'><br/>Drag & Drop Your Files Here!";
+        var successTxt = "All Files Are Being Uploaded Properly.";
+        var d = document,
+        b = d.getElementsByTagName('body')[0],
+        js = d.createElement('script');
+        js.type='text/javascript';
+        js.async=true;
+        js.src='//gdwidget.com/widget/upload/render.js';
+        b.appendChild(js);
+    </script>
+    <div id="gdwidget-upload-slot-24146db4eb"></div>   
