343bf574a14ba75e3ece52453f3d0451952bd303
grantimbo <grant.imbo@gmail.com>
2021-01-22 13:43:32 +0800

    add webp preview

--- functions.php
+++ functions.php
@@ -42,6 +42,24 @@ function webp_upload_mimes( $existing_mimes ) {
 }
 
 
+//** * Enable preview / thumbnail for webp image files.*/
+function webp_is_displayable($result, $path) {
+    if ($result === false) {
+        $displayable_image_types = array( IMAGETYPE_WEBP );
+        $info = @getimagesize( $path );
+
+        if (empty($info)) {
+            $result = false;
+        } elseif (!in_array($info[2], $displayable_image_types)) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+    }
+
+    return $result;
+}
+
 /*------------------------------------*\
 	Functions
 \*------------------------------------*/
@@ -539,6 +557,4 @@ remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
 // Add Filters
 add_filter('widget_text', 'shortcode_unautop'); // Remove <p> tags in Dynamic Sidebars (better!)
 add_filter( 'mime_types', 'webp_upload_mimes' );
-
-
-
+add_filter('file_is_displayable_image', 'webp_is_displayable', 10, 2);
