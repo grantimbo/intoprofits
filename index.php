558e1be0672f77782500538fc346232aca93a331
grantimbo <grant.imbo@gmail.com>
2021-01-30 23:06:55 +0800

    make use of rest API

--- index.php
+++ index.php
@@ -1,14 +1,189 @@
 <?php get_header(); ?>
 <main role="main" class="container blogpost-container">
-		<div class="wrap clear">
-			<section>
-				<?php get_template_part('template-parts/loop'); ?>
-				<div class="clearfix"></div>
-				<?php get_template_part('template-parts/pagination'); ?>
-			</section>
-			<aside>
-				<?php get_template_part('template-parts/metrics'); ?>
-			</aside>
-		</div>
+	<div class="wrap clear">
+		<section>
+			<div id="postsWrap"></div>
+			<div class="loader"></div>
+			<button id="load_more" style="display: none;" onclick="loadMore()">Load More Articles</button>
+		</section>
+		<aside>
+			<?php get_template_part('template-parts/metrics'); ?>
+		</aside>
+	</div>
 </main><!-- .container -->
+
+
+<script>
+
+	let currentPage = 2,
+		loadedPosts = 0;
+		totalPosts = null;
+
+	const loadPost = () => {
+
+		let postsApi = siteData.homeUrl + '/wp-json/wp/v2/posts',
+			postsWrap = document.getElementById('postsWrap'),
+			load_more = document.getElementById('load_more'),
+			loader = document.querySelector('.loader');
+
+			fetch(postsApi)
+				.then (
+					function(response) {
+
+						totalPosts = response.headers.get('X-WP-Total')
+
+						if (response.status !== 200) {
+							console.log('Looks like there was a problem.')
+							alert('Looks like there was a problem. Please reload the page or contact us')
+							return
+						}
+
+						response.json().then(function(postData) {
+							
+							loader.setAttribute('style', 'display: none;')
+							load_more.setAttribute('style', 'display: block;')
+							
+							loadedPosts = postData.length
+
+							postData.forEach(function (post) {
+
+								console.log(post)
+								
+								let post_title_raw = post.title.rendered,
+									post_title = post_title_raw.replace(/"/g, '&quot;');
+									post_description_raw = post.excerpt.rendered,
+									post_description = post_description_raw.replace(/"/g, '&quot;');
+									post_link = post.link,
+									post_thumbnail = null,
+									featured_media = post.thumbnail;
+
+									// if has thumbnail
+									if ( !post.ACF === false ) {
+
+										post_thumbnail = `<div class="blog-thumb video">
+															<a href="${post_link}" rel="${post_title}" class="play-button"></a>
+															<div class="comment-count"><div class="fb-comments-count" data-href="${post_link}">0</div><span>comments</span></div>
+															<div class="post-link">
+																<img class="trs-four" src="${featured_media}" width="700" height="394" alt="${post_title}">
+															</div>
+														</div>`;
+
+									} else {
+
+										post_thumbnail = `<div class="blog-thumb">
+															<div class="comment-count"><div class="fb-comments-count" data-href="${post_link}">0</div><span>comments</span></div>
+															<a href="${post_link}" class="post-link">
+																<img class="trs-four" src="${featured_media}" width="700" height="394" alt="${post_title}">
+															</a>
+														</div>`
+									}
+
+
+
+									postsWrap.innerHTML += `<article class="article-content clear">
+																<a href="${post_link}"><h2 class="article-title">${post_title}</h2></a>
+																<p class="article-author">Daniel Audunsson</p>
+																${post_thumbnail}
+																${post_description}
+																<a class="view-article" href="${post_link}">Read More</a>
+															</article>`
+								}
+							);
+
+
+						})
+					}
+				).catch(function(err) {
+					console.log('Fetch Error :-S', err)
+					alert('Looks like there was a problem. Please reload the page or contact us')
+				}
+			)
+	}
+	
+	const loadMore = () => {
+
+		let activePage = currentPage++
+			postsApi = siteData.homeUrl + '/wp-json/wp/v2/posts?page=' + activePage,
+			postsWrap = document.getElementById('postsWrap'),
+			load_more = document.getElementById('load_more'),
+			loader = document.querySelector('.loader');
+			
+			
+			loader.setAttribute('style', 'display: block;');
+
+			fetch(postsApi)
+				.then (
+					function(response) {
+
+						if (response.status !== 200) {
+							alert('Looks like there was a problem. Please reload the page or contact us')
+						}
+
+						response.json().then(function(postData) {
+							
+							loader.setAttribute('style', 'display: none;')
+							loadedPosts = loadedPosts + postData.length
+
+							if ( loadedPosts <= totalPosts ) {
+								load_more.setAttribute('style', 'display: none;')
+							}
+
+							postData.forEach(function (post) {
+								let post_title_raw = post.title.rendered,
+									post_title = post_title_raw.replace(/"/g, '&quot;');
+									post_description_raw = post.excerpt.rendered,
+									post_description = post_description_raw.replace(/"/g, '&quot;');
+									post_link = post.link,
+									post_thumbnail = null,
+									featured_media = post.thumbnail;
+
+									// if has thumbnail
+									if ( !post.ACF === false ) {
+
+										post_thumbnail = `<div class="blog-thumb video">
+															<a href="${post_link}" rel="${post_title}" class="play-button"></a>
+															<div class="comment-count"><div class="fb-comments-count" data-href="${post_link}">0</div><span>comments</span></div>
+															<div class="post-link">
+																<img class="trs-four" src="${featured_media}" width="700" height="394" alt="${post_title}">
+															</div>
+														</div>`;
+
+									} else {
+
+										post_thumbnail = `<div class="blog-thumb">
+															<div class="comment-count"><div class="fb-comments-count" data-href="${post_link}">0</div><span>comments</span></div>
+															<a href="${post_link}" class="post-link">
+																<img class="trs-four" src="${featured_media}" width="700" height="394" alt="${post_title}">
+															</a>
+														</div>`
+									}
+
+
+									postsWrap.innerHTML += `<article class="article-content clear">
+																<a href="${post_link}"><h2 class="article-title">${post_title}</h2></a>
+																<p class="article-author">Daniel Audunsson</p>
+																${post_thumbnail}
+																${post_description}
+																<a class="view-article" href="${post_link}">Read More</a>
+															</article>`
+								}
+							);
+
+
+						})
+					}
+				).catch(function(err) {
+					console.log('Fetch Error', err)
+					alert('Looks like there was a problem. Please reload the page or contact us')
+				}
+			)
+
+
+	}
+
+	loadPost();
+	
+
+</script>
+
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -2,58 +2,27 @@
 <main role="main" class="container blogpost-container">
 	<div class="wrap clear">
 		<section>
-			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-					<h2 class="article-title"><?php the_title(); ?></h2>
-					<p class="article-author"><div class="fb-like" data-href="<?php the_permalink(); ?>" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="true"></div> <?php the_author(); ?></p>
-
-					<?php if( get_field('video') ): ?>
-						<div class="embed-container">
-							<?php the_field('video'); ?>
-						</div>
-					<?php else: ?>
-						<?php if(has_post_thumbnail()) : the_post_thumbnail(); else :?>
-							<img class="trs-four" src="<?php bloginfo('template_url'); ?>/img/no-thumb.png" alt="Thumbnail does not exist">
-						<?php endif; ?>
-					<?php endif; ?>
-
-					<?php the_content(); // Dynamic Content ?>
-
-					<p class="transcript-title">Transcript / <a href="" class="mp3-btn">Mp3</a></p>
-					<div class="mp3-wrap">
-						<?php if( get_field('mp3') ): ?>
-						    <audio controls>
-							  <source src="<?php the_field('mp3'); ?>" type="audio/mpeg">
-								Your browser does not support the audio element.
-							</audio>
-						<?php else: ?>
-							<h4>No MP3/Audio File</h4>
-						<?php endif; ?>
-					</div>
-					
-					<?php if( get_field('transcript') ): ?>
-						<div class="video-transcript">
-						    <?php the_field('transcript'); ?>
-						    <span class="transcript-readmore-wrap">
-								<a class="button transcript-readmore">Read More</a>
-							</span>
-						</div>
-					<?php else: ?>
-						<div class="video-transcript no-transcript">
-							<h4>No Transcript</h4>
-						</div>
-					<?php endif; ?>
-						
-
-					
-					<?php edit_post_link(); ?>
-
-
-				</article><!-- /article -->
-
-			<?php endwhile; endif; ?>
-
+            <div class="loader"></div>
+            <article style="display: none;">
+                <h2 class="article-title"></h2>
+                <p class="article-author">
+                    <div class="fb-like" 
+                        data-href="<?php the_permalink(); ?>"
+                        data-layout="button_count"
+                        data-action="like"
+                        data-size="small"
+                        data-show-faces="false"
+                        data-share="true">
+                    </div>
+                    <span>Daniel Audunsson</span>
+                </p>
+                <div id="featured_media"></div>
+                <div id="content"></div>
+                <p class="transcript-title">Transcript / <a href="" class="mp3-btn">Mp3</a></p>
+                <div class="mp3-wrap"></div>
+                <div id="trascript_wrap"></div>
+        
+            </article>
 		</section>
 		<aside>
 			<?php get_template_part('template-parts/metrics'); ?>
@@ -66,4 +35,62 @@
 		</section>
 	</div>
 </main>
+
+<script>
+	const loadPost = () => {
+
+			let postAPI = siteData.homeUrl + '/wp-json/wp/v2/posts/<?php echo get_the_ID(); ?>',
+				article_title = document.querySelector('.article-title'),
+				content = document.querySelector('#content'),
+				article = document.querySelector('article'),
+				featured_media = document.querySelector('#featured_media'),
+				mp3_wrap = document.querySelector('.mp3-wrap'),
+				trascript_wrap = document.querySelector('#trascript_wrap'),
+				loadingResults = document.querySelector(".loader");
+
+                console.log(postAPI)
+
+			fetch(postAPI)
+				.then (
+					function(response) {
+
+						if (response.status !== 200) {
+							console.log('Looks like there was a problem.')
+							alert('Looks like there was a problem. Please reload the page or contact us')
+							return
+						}
+
+						response.json().then(function(post) {
+							
+
+							console.log(post)
+
+							article_title.innerHTML = post.title.rendered
+							content.innerHTML = post.content.rendered
+							article.setAttribute('style', 'display: block;')
+
+							// check video
+							if ( !post.ACF === false ) {
+								featured_media.innerHTML = '<div class="embed-container">' + post.ACF.video + '</div>'
+								mp3_wrap.innerHTML = `<audio controls><source src="`+ post.ACF.mp3 +`" type="audio/mpeg">Your browser does not support the audio element.</audio>`
+								trascript_wrap.innerHTML = `<div class="video-transcript">` + post.ACF.transcript + `<span class="transcript-readmore-wrap"><a class="button transcript-readmore">Read More</a></span></div>`
+							} else {
+								featured_media.innerHTML = '<img class="trs-four" src="'+ siteData.templateUrl +'/img/no-thumb.png" alt="Thumbnail does not exist">'
+								mp3_wrap.innerHTML = '<h4>No MP3/Audio File</h4>'
+								trascript_wrap.innerHTML = '<div class="video-transcript no-transcript"><h4>No Transcript</h4></div>'
+							}
+
+							loadingResults.setAttribute('style', 'display: none;')
+
+						})
+					}
+				).catch(function(err) {
+					console.log('Fetch Error :-S', err)
+					alert('Looks like there was a problem. Please reload the page or contact us')
+				}
+			)
+        }
+
+        loadPost();
+</script>
 <?php get_footer(); ?>
