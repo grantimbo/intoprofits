f3e5f66f94917fef6fb8491f8935b42f114e44b6
grantimbo <grant.imbo@gmail.com>
2021-03-17 02:24:58 +0800

    add Products page template

---
+++ template-products.php
@@ -0,0 +1,28 @@
+<?php /* Template Name: Products */ get_header(); ?>
+
+<main role="main" class="container products-container">
+	<div class="wrap">
+
+        <div class="products-head">
+            <h1>Cutting-Edge Products</h1>
+            <p>We teach the things that no-one else is teaching. All of our products are built from the ground-up with our own proprietary systems and processes. They are NEW, innovative and give our students a true competitive advantage.</p>
+        </div>
+
+	</div>
+</main>
+
+<section class="products-apex-seller-wrap">
+	<div class="wrap">
+
+        <img src="<?php echo bloginfo('template_url'); ?>/img/apex-seller-product-graphic.png" alt="Apex Seller">
+        <div class="products-apex-graphic">
+            <h1>Apex Seller™</h1>
+            <b>Stage: Scaling Up <span class="scaleProgbar"></span></b>
+            <p>How to scale into 7- and 8-figures by leveraging professional grade systems & data, full-scale PPC advertising, unique products and a world-class team.</p>
+            <a href="<?php echo site_url(); ?>/products/apex-seller" class="button">Learn More</a>
+        </div>
+
+	</div>
+</section>
+
+<?php get_footer(); ?>
