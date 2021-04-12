9b7c2c973d3de54514dc1b40cad027f4e6dc1a49
grantimbo <grant.imbo@gmail.com>
2021-04-13 01:29:57 +0800

    update button

--- archive-results.php
+++ archive-results.php
@@ -19,7 +19,7 @@
         
         <!-- review footer -->
         <div class="reviews-footer">
-            <a href="<?php echo get_home_url(); ?>/case-study" class="button go-intoprofits">ACCESS OUR $183M+ CASE STUDY</a>
+            <a href="<?php echo get_home_url(); ?>/case-study" class="button go-intoprofits">ACCESS OUR $200M+ CASE STUDY</a>
         </div>
             
     </div>
